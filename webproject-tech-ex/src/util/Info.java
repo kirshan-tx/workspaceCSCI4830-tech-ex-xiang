dddc4a1ab90c0d8f4dc88d2e8eb62174f7900cb6
Kirshan Tzer Xiang <kirshantzerxiang@gmail.com>
2023-10-08 09:50:00 -0500

    Added info.java and updated Search HTML to use "Expenses" to search

---
+++ webproject-tech-ex/src/util/Info.java
@@ -0,0 +1,7 @@
+package util;
+
+public interface Info {
+   public String projectName = "webproject-tech-ex";
+   public String searchWebName = "simpleSearchHB.html";
+   public String insertWebName = "simpleInsertHB.html";
+}
